b8d4523d40c15014dec365338dabe6141a1e7cbc
Texas_404 <texasholdem404@gmail.com>
2024-03-14 12:06:55 +0800

    Del buy

--- contracts/texas404.sol
+++ contracts/texas404.sol
@@ -8,19 +8,9 @@ import "./texas_evaluate.sol";
 // https://texas404.com
 contract Texas404 is ERC404 {
     string public baseTokenURI;
-    uint stage = 0;
-    uint stageOnePrice = 0.0003 ether;
-    uint stageTwoPrice = 0.0005 ether;
-    uint stageOneAmount = 77968;
-    uint stageTwoAmount = 129948;
 
     constructor() ERC404("Texas404", "txc", 18, 2598960, msg.sender) {
-        balanceOf[msg.sender] = (2598960 - stageOneAmount - stageTwoAmount) * 10 ** 18;
-        balanceOf[address(this)] = (stageOneAmount + stageTwoAmount) * 10 ** 18;
-    }
-
-    function setStage(uint _stage) public onlyOwner {
-        stage = _stage;
+        balanceOf[msg.sender] = 2598960* 10 ** 18;
     }
 
     function withdraw(uint256 value)  public onlyOwner {
@@ -31,24 +21,6 @@ contract Texas404 is ERC404 {
 
     function withdrawETH() public onlyOwner {
         payable(rewardAddr()).transfer(address(this).balance);
-    }
-
-    function buy(uint256 amount) public payable {
-        require(stage != 0, 'Market closed!');
-
-        if (stage == 1) {
-            require(stageOneAmount >= amount, 'Insufficient quota for stage 1!');
-            require((amount * stageOnePrice) == msg.value, 'Incorrect ETH value');
-            _transfer(address(this), msg.sender, amount *_getUnit());
-            stageOneAmount -= amount;
-        }
-
-        if (stage == 2) {
-            require(stageTwoAmount >= amount, 'Insufficient quota for stage 2!');
-            require((amount * stageTwoPrice) == msg.value, 'Incorrect ETH value');
-            _transfer(address(this), msg.sender, amount *_getUnit());
-            stageTwoAmount -= amount;
-        }
     }
 
     function setTokenURI(string memory _tokenURI) public onlyOwner {
